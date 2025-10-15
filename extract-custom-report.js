d33df09a9d147d1da568f498d42bf60e19e1a3cf
ioedeveloper <daviddisu8@gmail.com>
2025-10-15 07:17:47 +0100

    Refactor custom report extractor

--- extract-custom-report.js
+++ extract-custom-report.js
@@ -2,9 +2,9 @@ const fs = require('fs').promises
 
 require('dotenv').config()
 
-const getData = async (date) => {
+const getData = async (date, reportUniqueId) => {
     console.log(`getting ${date}`)
-    const ret = await fetch(`https://matomo.remix.live/matomo/index.php?module=API&format=JSON&idSite=3&period=day&date=${date}&method=CustomReports.getCustomReport&idCustomReport=9&reportUniqueId=CustomReports_getCustomReport_idCustomReport--9&flat=1&showMetadata=0&token_auth=${process.env.MATOMO_API_KEY}&filter_limit=-1`)
+    const ret = await fetch(`https://matomo.remix.live/matomo/index.php?module=API&format=JSON&idSite=3&period=day&date=${date}&method=CustomReports.getCustomReport&idCustomReport=${reportUniqueId}&reportUniqueId=CustomReports_getCustomReport_idCustomReport--${reportUniqueId}&showMetadata=0&token_auth=${process.env.MATOMO_API_KEY}&flat=1&filter_limit=-1`)
 
     let retJson = []
     try {
@@ -14,20 +14,28 @@ const getData = async (date) => {
         console.log(e)
         return
     }
-    await fs.writeFile(`data/raw-${date}.json`, JSON.stringify(retJson, null, '\t'))
+    await fs.writeFile(`data/${reportUniqueId}/raw-${date}.json`, JSON.stringify(retJson, null, '\t'))
 }
 
-const run = async (startDate, endDate) => {
-    let currentDate = new Date(startDate);
+const run = async (startDate, endDate, reportUniqueId) => {
+    let currentDate = new Date(startDate)
+    fs.mkdir(`data/${reportUniqueId}`, { recursive: true })
+    console.log(`--- getting custom report with id ${reportUniqueId} ---`)
     while (currentDate <= endDate) {
-        const formattedDate = currentDate.toISOString().split('T')[0];
-        await getData(formattedDate);
-        currentDate.setDate(currentDate.getDate() + 1);
+        const formattedDate = currentDate.toISOString().split('T')[0]
+
+        await getData(formattedDate, reportUniqueId)
+        currentDate.setDate(currentDate.getDate() + 1)
     }
 }
+
 const startDate = new Date(2025, 8, 25)
-console.log(startDate)
 const endDate = new Date()
 
-run(startDate, endDate).catch(console.error);
+const main = async () => {
+    await run(startDate, endDate, 9)
+    await run(startDate, endDate, 14)
+}
+
+main().catch(console.error)
 
